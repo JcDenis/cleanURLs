b2e74ff29d04c968e966e6fab8542764a27160e9
Jean-Christian Denis <contact@jcdenis.fr>
2023-08-06 19:21:47 +0200

    upgrade to Dotclear 2.27

---
+++ src/My.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @brief cleanURLs, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugin
+ *
+ * @author Pierre Rudloff and contributors
+ *
+ * @copyright Jean-Christian Denis
+ * @copyright GPL-3.0 https://www.gnu.org/licenses/gpl-3.0.html
+ */
+declare(strict_types=1);
+
+namespace Dotclear\Plugin\cleanURLs;
+
+use Dotclear\Module\MyPlugin;
+
+class My extends MyPlugin
+{
+}
